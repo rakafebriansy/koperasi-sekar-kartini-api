741f83ea75e60b9a4a9f13ed8c6a31e7e774637e
rakafebriansy <rakafebrian19@gmail.com>
2025-11-23 07:06:25 +0700

    chore: clear duplicate swagger annotations

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,24 +2,6 @@
 
 namespace App\Http\Controllers;
 
-/**
- * @OA\Info(
- *     version="1.0.0",
- *     title="API Koperasi Sekar Kartini",
- *     description="API untuk aplikasi koperasi, termasuk autentikasi, transaksi, dan manajemen anggota.",
- *     termsOfService="http://example.com/terms/",
- *     @OA\Contact(
- *         email="support@example.com",
- *         name="Support Team",
- *         url="http://example.com/contact"
- *     ),
- *     @OA\License(
- *         name="MIT",
- *         url="https://opensource.org/licenses/MIT"
- *     )
- * )
- */
-
 abstract class Controller
 {
     //
