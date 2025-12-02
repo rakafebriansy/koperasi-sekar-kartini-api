eac86e15fdd3d5e11350dbf567f64ba8d5e8a8e7
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-12-02 18:48:24 +0700

    fix: set null when delete and make id primary key

--- database/migrations/2025_12_02_000000_create_meetings_table.php
+++ database/migrations/2025_12_02_000000_create_meetings_table.php
@@ -12,15 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('meetings', function (Blueprint $table) {
-            $table->id();
+            $table->id()->primary();
             $table->enum('meeting_type', ['Activity', 'Routine Meeting']);
             $table->date('date');
             $table->time('time');
             $table->string('location');
             $table->string('photo');
             $table->text('description');
-            $table->unsignedBigInteger('group_id');
-            $table->foreign('group_id')->references('id')->on('member_groups')->onDelete('cascade');
+            $table->unsignedBigInteger('group_id')->nullable();
+            $table->foreign('group_id')->references('id')->on('member_groups')->onDelete('set null');
             $table->timestamps();
         });
     }
--- database/migrations/2025_12_02_000001_create_users_meetings_table.php
+++ database/migrations/2025_12_02_000001_create_users_meetings_table.php
@@ -12,13 +12,13 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users_meetings', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->unsignedBigInteger('meeting_id');
+            $table->id()->primary();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->unsignedBigInteger('meeting_id')->nullable();
             $table->boolean('status')->default(false);
 
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->foreign('meeting_id')->references('id')->on('meetings')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('meeting_id')->references('id')->on('meetings')->onDelete('set null');
 
             $table->timestamps();
         });
