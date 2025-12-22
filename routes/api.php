c4ca273dddd8d6fd76e603b7f923a2dba128c14e
rakafebriansy <rakafebrian19@gmail.com>
2025-12-22 12:43:27 +0700

    test: include test fcm route into auth middleware

--- routes/api.php
+++ routes/api.php
@@ -13,27 +13,23 @@ use App\Http\Controllers\WorkAreaController;
 use App\Jobs\TestFcmNotificationJob;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test-fcm', function () {
-    TestFcmNotificationJob::dispatch(auth()->id());
-    return response()->json(['status' => 'sent']);
-});
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'registerGroupMember']);
 
 Route::get('/file/{path}', [FileController::class, 'show'])
-    ->where('path', '.*');
+->where('path', '.*');
 
 Route::get('/download/{path}', [FileController::class, 'download'])
-    ->where('path', '.*')
-    ->name('download.file');
+->where('path', '.*')
+->name('download.file');
 
 Route::get('/work-areas', [WorkAreaController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/refresh', [AuthController::class, 'refreshToken']);
     Route::post('/logout', [AuthController::class, 'logout']);
-
+    
     Route::get('/member-growth', [ReportController::class, 'memberGrowth']);
     Route::get('/dashboard-stats', [ReportController::class, 'dashboardStats']);
     Route::get(
@@ -44,14 +40,14 @@ Route::middleware('auth:sanctum')->group(function () {
         '/loan-distribution-chart',
         [LoanController::class, 'distribution']
     );
-
+    
     Route::prefix('/work-areas')->middleware(['role:admin'])->group(function () {
         Route::post('/', [WorkAreaController::class, 'store']);
         Route::get('{id}', [WorkAreaController::class, 'show']);
         Route::patch('{id}', [WorkAreaController::class, 'update']);
         Route::delete('{id}', [WorkAreaController::class, 'destroy']);
     });
-
+    
     Route::get('/users', [UserController::class, 'index']);
     Route::prefix('users')->middleware(['role:admin'])->group(function () {
         Route::post('/', [UserController::class, 'store']);
@@ -61,10 +57,10 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::patch('/{id}/groups/{groupId}', [UserController::class, 'updateGroup']);
         Route::patch('/{id}/activate', [UserController::class, 'activate']);
     });
-
+    
     Route::get('/unlisted-members', [UserController::class, 'unlistedMembers']);
     Route::get('/inactive-members', [UserController::class, 'inactiveMembers']);
-
+    
     Route::get('/groups', [GroupController::class, 'index']);
     Route::get('/groups/{groupId}', [GroupController::class, 'show']);
     Route::get('/groups/{groupId}/reports', [ReportController::class, 'index']);
@@ -72,11 +68,11 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::post('/', [GroupController::class, 'store']);
         Route::put('{groupId}', [GroupController::class, 'update']);
         Route::delete('{groupId}', [GroupController::class, 'destroy']);
-
+        
         Route::patch('{groupId}/update-fund-amount', [GroupController::class, 'updateFundAmount']);
         Route::patch('{groupId}/facilitator/{userId}', [GroupController::class, 'updateFacilitator']);
         Route::patch('{groupId}/chairman/{userId}', [GroupController::class, 'updateChairman']);
-
+        
         Route::prefix('/{groupId}/reports')->group(function () {
             Route::post('/', [ReportController::class, 'store']);
             Route::get('{reportId}', [ReportController::class, 'show']);
@@ -84,7 +80,7 @@ Route::middleware('auth:sanctum')->group(function () {
             Route::delete('{reportId}', [ReportController::class, 'destroy']);
         });
     });
-
+    
     Route::prefix('meetings')->group(function () {
         Route::get('/', [MeetingController::class, 'index']);
         Route::get('/upcoming', [MeetingController::class, 'upcomingMeetings']);
@@ -93,7 +89,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::put('{id}', [MeetingController::class, 'update']);
         Route::delete('{id}', [MeetingController::class, 'destroy']);
     });
-
+    
     Route::get('/loans', [LoanController::class, 'index']);
     Route::get('/loans/sum-by-month', [LoanController::class, 'sumByMonth']);
     Route::prefix('loans')->middleware('role:admin,employee')->group(function () {
@@ -102,7 +98,7 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::put('{id}', [LoanController::class, 'update']);
         Route::delete('{id}', [LoanController::class, 'destroy']);
     });
-
+    
     Route::get('/savings', [SavingsController::class, 'index']);
     Route::get('/savings/sum-by-month', [SavingsController::class, 'sumByMonth']);
     Route::prefix('savings')->middleware('role:admin,employee')->group(function () {
@@ -110,6 +106,11 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::get('{id}', [SavingsController::class, 'show']);
         Route::put('{id}', [SavingsController::class, 'update']);
         Route::delete('{id}', [SavingsController::class, 'destroy']);
+    });
+    
+    Route::get('/test-fcm', function () {
+        TestFcmNotificationJob::dispatch(auth()->id());
+        return response()->json(['status' => 'sent']);
     });
 });
 
